7148da3c27e28f71b72b33e810596d5397cce452
Gabriel Magpantay <rielmagpantay@gmail.com>
2026-05-10 15:39:32 +0000

    Try fixing this error: `Runtime ReferenceError: Badge is not defined. Er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ import {
 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
 import { useFirestore, useMemoFirebase, useUser, useAuth, useDoc } from '@/firebase';
 import { doc } from 'firebase/firestore';
 import { signOut } from 'firebase/auth';
